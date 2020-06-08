382d968a0cc20a3b9b8aecbd1e629c8c11510a9d
Joshua Baldwin <JB@Karlies-MBP.attlocal.net>
2020-06-07 20:55:55 -0500

    adding configurables

--- log/log.go
+++ log/log.go
@@ -1,9 +1,42 @@
 package log
 
 import (
+	"os"
 	"path"
 	"runtime"
+
+	logrusStack "github.com/Gurpartap/logrus-stack"
+	"github.com/sirupsen/logrus"
 )
+
+// Entry struct
+var Entry *logrus.Entry
+
+// Logger struct
+type Logger struct {
+	Name  string
+	Level logrus.Level
+	Stack bool
+}
+
+// Configure logger
+func Configure(conf Logger) {
+	callerLevels := []logrus.Level{logrus.PanicLevel}
+	stackLevels := logrus.AllLevels
+
+	if !conf.Stack {
+		stackLevels = []logrus.Level{logrus.PanicLevel}
+	}
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(conf.Level)
+	logrus.AddHook(logrusStack.NewHook(callerLevels, stackLevels))
+
+	Entry = logrus.WithFields(logrus.Fields{
+		"application": conf.Name,
+	})
+}
 
 // Line number
 func Line() int {
