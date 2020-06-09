5da6b0be7b4fba020d4ce3071312394085f3ad00
Joshua Baldwin <JB@Karlies-MBP.attlocal.net>
2020-06-09 08:19:28 -0500

    making log loosely coupled

--- log/log.go
+++ log/log.go
@@ -18,22 +18,28 @@ type Fields map[string]interface{}
 // Logger struct
 type Logger struct {
 	Name  string
-	Level logrus.Level
+	Level string
 	Stack bool
 }
 
 // Configure logger
 func Configure(conf Logger) {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
+
+	lvl, err := logrus.ParseLevel(conf.Level)
+	if err != nil {
+		lvl = logrus.ErrorLevel
+	}
+
+	logrus.SetLevel(lvl)
+
 	callerLevels := []logrus.Level{logrus.PanicLevel}
 	stackLevels := logrus.AllLevels
-
 	if !conf.Stack {
 		stackLevels = []logrus.Level{logrus.PanicLevel}
 	}
 
-	logrus.SetFormatter(&logrus.JSONFormatter{})
-	logrus.SetOutput(os.Stdout)
-	logrus.SetLevel(conf.Level)
 	logrus.AddHook(logrusStack.NewHook(callerLevels, stackLevels))
 
 	Entry = logrus.WithFields(logrus.Fields{
@@ -41,34 +47,15 @@ func Configure(conf Logger) {
 	})
 }
 
-// Add fields to log
-func Add(fields Fields) {
-	lFields := make(logrus.Fields)
-
-	for k, v := range fields {
-		lFields[k] = v
-	}
-
-	Entry = logrus.WithFields(lFields)
-}
-
-// Details of log
-func Details() {
-	Entry = logrus.WithFields(logrus.Fields{
-		"line": line(),
-		"file": file(),
-	})
-}
-
 // Line number
-func line() int {
-	_, _, line, _ := runtime.Caller(2)
+func Line() int {
+	_, _, line, _ := runtime.Caller(1)
 	return line
 }
 
 // File name
-func file() string {
-	_, filePath, _, _ := runtime.Caller(2)
+func File() string {
+	_, filePath, _, _ := runtime.Caller(1)
 	file := path.Base(filePath)
 
 	return file
