0b7f70a4b9b3eeef69eb4d66136ce072c61da6d4
Joshua Baldwin <JB@Karlies-MBP.attlocal.net>
2020-06-10 12:40:02 -0500

    fixing URL bug with adding paths and refactoring

--- client/client.go
+++ client/client.go
@@ -17,80 +17,61 @@ type Client struct {
 }
 
 // New client
-func New(uri string, timeout int) Client {
+func New(rel string, timeout int) (Client, error) {
+	uri, err := url.Parse(rel)
+	if err != nil {
+		return Client{}, fmt.Errorf("could not create client, %s", err)
+	}
+
 	return Client{
-		URL: &url.URL{
-			Host: uri,
-		},
+		URL: uri,
 		Client: &http.Client{
 			Timeout: time.Duration(timeout) * time.Second,
 		},
-	}
+	}, err
 }
 
 // Post request
-func (c *Client) Post(endpoint string, headers, parameters map[string]string, body io.Reader) (resp response.Response, err error) {
-	url := fmt.Sprintf("%s%s", c.URL.Host, endpoint)
-	req, err := http.NewRequest(http.MethodPost, url, body)
-	if err != nil {
-		err = fmt.Errorf("problem occured invoking %s: %s, %s", http.MethodPost, url, err)
-		return
-	}
-
-	req = prepRequest(req, headers, parameters)
-	return c.do(req)
+func (c *Client) Post(path string, headers, parameters map[string]string, body io.Reader) (response.Response, error) {
+	return c.do(http.MethodPut, path, headers, parameters, body)
 }
 
 // Get request
-func (c *Client) Get(endpoint string, headers, parameters map[string]string) (resp response.Response, err error) {
-	url := fmt.Sprintf("%s%s", c.URL.Host, endpoint)
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		err = fmt.Errorf("problem occured invoking %s: %s, %s", http.MethodGet, url, err)
-		return
-	}
-
-	req = prepRequest(req, headers, parameters)
-	return c.do(req)
+func (c *Client) Get(path string, headers, parameters map[string]string) (response.Response, error) {
+	return c.do(http.MethodPut, path, headers, parameters, nil)
 }
 
 // Put request
-func (c *Client) Put(endpoint string, headers, parameters map[string]string, body io.Reader) (resp response.Response, err error) {
-	url := fmt.Sprintf("%s%s", c.URL.Host, endpoint)
-	req, err := http.NewRequest(http.MethodPut, url, body)
-	if err != nil {
-		err = fmt.Errorf("problem occured invoking %s: %s, %s", http.MethodPut, url, err)
-		return
-	}
-
-	req = prepRequest(req, headers, parameters)
-	return c.do(req)
+func (c *Client) Put(path string, headers, parameters map[string]string, body io.Reader) (response.Response, error) {
+	return c.do(http.MethodPut, path, headers, parameters, body)
 }
 
 // Delete request
-func (c *Client) Delete(endpoint string, headers, parameters map[string]string) (resp response.Response, err error) {
-	url := fmt.Sprintf("%s%s", c.URL.Host, endpoint)
-	req, err := http.NewRequest(http.MethodDelete, url, nil)
-	if err != nil {
-		err = fmt.Errorf("problem occured invoking %s: %s, %s", http.MethodDelete, url, err)
-		return
-	}
-
-	req = prepRequest(req, headers, parameters)
-	return c.do(req)
+func (c *Client) Delete(path string, headers, parameters map[string]string) (response.Response, error) {
+	return c.do(http.MethodDelete, path, headers, parameters, nil)
 }
 
 // helper functions
-func (c *Client) do(req *http.Request) (resp response.Response, err error) {
+func (c *Client) do(method, path string, headers, parameters map[string]string, body io.Reader) (response.Response, error) {
+	resp := response.Response{}
+
+	c.URL.Path = path
+	req, err := http.NewRequest(method, c.URL.String(), body)
+	if err != nil {
+		return resp, fmt.Errorf("problem occured building %s request: %s, %s", method, c.URL.String(), err)
+
+	}
+
+	req = prepRequest(req, headers, parameters)
 	r, err := c.Client.Do(req)
 	if err != nil {
-		return
+		return resp, fmt.Errorf("problem occured with request: %s", err)
 	}
 	defer r.Body.Close()
 
 	resp.Save(r)
 
-	return
+	return resp, nil
 }
 
 func prepRequest(req *http.Request, headers, parameters map[string]string) *http.Request {
